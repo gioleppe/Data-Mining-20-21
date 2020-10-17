0f9a6aae8b40995f188ab486c8227b0409951cea
Tommaso Colella <tommasocolella95@gmail.com>
2020-10-17 17:20:35 +0200

    serialized supermarket with nan values
    set up correct dtypes for the dataframe

---
+++ python/interactive_cells.py
@@ -0,0 +1,13 @@
+# %%
+print("Make america great again")
+# %%
+
+# %%
+import matplotlib.pyplot as plt
+import numpy as np
+
+data = np.random.rand(2,10)
+
+plt.scatter(data[0], data[1])
+plt.show()
+# %%
